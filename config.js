d7838d0b21ec42244bea6ffeb53a3bf06c2d2c93
Vincenzo675 <vinceth349@gmail.com>
2024-10-14 20:06:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
   ANTIBOT_MSG: process.env.ANTIBOT_MSG || "_*Bot Not allowed in this group*_",
   ANTIWORD_MSG: process.env.ANTIWORD_MSG || "_*antiword deleted successfully*_",
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_a0ac_24b5_c78d_5b79_bdf0_e029_539f_e86d',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_504d_3464_0b3a_1db9_f947_84e7_d1f8_1ced',
   LANG: 'EN',
   SETVV: process.env.SETVV || 'DM',
   ELEVENLABS: process.env.ELEVENLABS,
